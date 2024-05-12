a2dc56c3a973e7f876d1f5747c5618cd286adf70
ogunsola <olubukola@me.com>
2024-05-12 18:30:21 -0400

    added ros2 control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -14,10 +14,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='my_bot' #<--- CHANGE ME
+    package_name='my_bot' 
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
